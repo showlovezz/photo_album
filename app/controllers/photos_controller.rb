af4968a383e622b40670afb65c885f58e4eaab51
vita <dragic.hsieh@gmail.com>
2018-01-20 13:43:26 +0800

    Modify CRUD and controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,6 @@
 class PhotosController < ApplicationController
+	before_action :set_photo, :only => [:show, :edit, :update, :destroy]
+
 	def index
 		@photos = Photo.all
 	end
@@ -9,28 +11,32 @@ class PhotosController < ApplicationController
 
 	def create
 		@photo = Photo.new(photo_params)
-		@photo.save
-
-		redirect_to photos_url
+		if @photo.save
+			redirect_to photos_url
+		else
+			render :action => :new
+		end		
 	end
 
 	def show
-		@photo = Photo.find(params[:id])
+		set_photo
 	end
 	
 	def edit
-		@photo = Photo.find(params[:id])
+		set_photo
 	end
 	
 	def update
-		@photo = Photo.find(params[:id])
-		@photo.update_attributes(photo_params)
-
-		redirect_to photo_path(@photo)
+		set_photo
+		if @photo.update_attributes(photo_params)
+			redirect_to photo_path(@photo)
+		else
+			render :action => :edit
+		end		
 	end
 
 	def destroy
-		@photo = Photo.find(params[:id])
+		set_photo
 		@photo.destroy
 
 		redirect_to photos_url
@@ -42,4 +48,10 @@ class PhotosController < ApplicationController
 		params.require(:photo).permit(:title, :date, :description, :file)
 	end	
 
+	private
+
+	def set_photo
+		@photo = Photo.find(params[:id])
+	end
+		
 end
